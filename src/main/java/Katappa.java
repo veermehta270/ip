03b12a679e595d3d3fd59a8e738a2076b5e4a976
Veer Mehta <e1357046@u.nus.edu>
2026-01-17 20:48:23 +0800

    Name chatbot as Katappa and give greeting message

---
+++ src/main/java/Katappa.java
@@ -0,0 +1,18 @@
+public class Katappa {
+    public static void main(String[] args) {
+        String name = "Katappa";
+        String line = "___________________________________________________________";
+
+        System.out.println(line);
+        System.out.println("Hello! I'm Katappa");
+        System.out.println("What can I do for you?");
+        System.out.println();
+
+
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println();
+        System.out.println(line);
+
+    }
+}
