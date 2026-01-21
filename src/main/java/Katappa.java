296bad997e79861ba76a36145316bf9621fb39d3
Veer Mehta <e1357046@u.nus.edu>
2026-01-21 16:44:56 +0800

    Add Mark and Unmark functionality
    Create a new class named Task which represents the tasks assigned.
    Use task object to store, mark or unmark tasks assigned.

--- src/main/java/Katappa.java
+++ src/main/java/Katappa.java
@@ -4,24 +4,88 @@ public class Katappa {
 
     public static final String NAME = "Katappa";
     public static String LINE = "___________________________________________________________";
-    private static String[] tasksList = new String[100];
+    private static Task[] tasksList = new Task[100];
     private static int totalTasks = 0;
+
+    private static void markOrUnmark(int index, String command) {
+        if(totalTasks == 0) {
+            System.out.println("\t" + LINE);
+            System.out.println("\t" + "There are no tasks yet My Lord . Please add tasks, I am at your service.");
+            System.out.println("\t" + LINE);
+        }
+
+        else if((index > totalTasks) || (index < 1)) {
+            System.out.println("\t" + LINE);
+            System.out.println("\t" + "My Lord, there is no such task yet, please enter a valid task number. I am at your service.");
+            System.out.println("\t" + LINE);
+        }
+
+        else if(command.equalsIgnoreCase("mark")) {
+            Task task = tasksList[index - 1];
+            if(task.isDone()) {
+                System.out.println("\t" + LINE);
+                System.out.println("\t" + "My Lord, the task already has the status of Done!");
+                System.out.println("\t" + " " + task.getStatusIcon() + " " + task.getDescription());
+                System.out.println("\t" + LINE);
+
+            }
+            else {
+                task.markAsDone();
+                System.out.println("\t" + LINE);
+                System.out.println("\t" + "Nice! I've marked this task as done:");
+                System.out.println("\t" + " " + task.getStatusIcon() + " " + task.getDescription());
+                System.out.println("\t" + LINE);
+            }
+
+        }
+
+        else if(command.equalsIgnoreCase("unmark")) {
+            Task task = tasksList[index - 1];
+
+            if(!task.isDone) {
+                System.out.println("\t" + LINE);
+                System.out.println("\t" + "My Lord, the task already has the status of Not Done!");
+                System.out.println("\t" + " " + task.getStatusIcon() + " " + task.getDescription());
+                System.out.println("\t" + LINE);
+
+            }
+            else {
+                task.markAsNotDone();
+                System.out.println("\t" + LINE);
+                System.out.println("\t" + "OK, I've marked this task as not done yet:");
+                System.out.println("\t" + " " + task.getStatusIcon() + " " + task.getDescription());
+                System.out.println("\t" + LINE);
+            }
+
+        }
+
+
+    }
+
+    private static boolean isInteger(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 
     private static void printTaskList() {
 
         if(totalTasks == 0) {
             System.out.println("\t" + LINE);
-            System.out.println("\t" + "No tasks yet master. Please add tasks, I am at your service.");
+            System.out.println("\t" + "No tasks yet my Lord. Please add tasks, I am at your service.");
             System.out.println("\t" + LINE);
 
         }
-
+        System.out.println("\t" + LINE);
         for(int i = 0; i < totalTasks; i++) {
-            System.out.println("\t" + LINE);
-            System.out.println("\t" + (i + 1) + ". " + tasksList[i]);
-            System.out.println("\t" + LINE);
 
+            System.out.println("\t" + (i + 1) + ". " + tasksList[i].getStatusIcon() + " " + tasksList[i].getDescription());
         }
+        System.out.println("\t" + LINE);
+
     }
 
     public static void main(String[] args) {
@@ -39,19 +103,40 @@ public class Katappa {
 
        while((!input.equalsIgnoreCase("bye"))) {
 
-           if(input.equalsIgnoreCase("list")) {
+           if (input.equalsIgnoreCase("list")) {
                printTaskList();
                input = in.nextLine();
                continue;
+           } else if (input.split(" ")[0].equalsIgnoreCase("mark")) {
+
+               if (isInteger(input.split(" ")[1])) {
+                   markOrUnmark(Integer.parseInt(input.split(" ")[1]), "mark");
+                   input = in.nextLine();
+                   continue;
+
+               }
+
+           } else if (input.split(" ")[0].equalsIgnoreCase("unmark")) {
+
+               if (isInteger(input.split(" ")[1])) {
+                   markOrUnmark(Integer.parseInt(input.split(" ")[1]), "unmark");
+                   input = in.nextLine();
+                   continue;
+
+               }
            }
+
+
 
            System.out.println("\t" + LINE);
            System.out.println("\t" + "Added: " + input);
            System.out.println("\t" + LINE);
-           tasksList[totalTasks] = input;
+           tasksList[totalTasks] = new Task(input);
            totalTasks++;
            input = in.nextLine();
+
        }
+
 
         System.out.println("\t" + LINE);
         System.out.println("\t" + "Rest peacefully my Lord, Katappa's sword stays ready for your return.");
---
+++ src/main/java/Task.java
@@ -0,0 +1,37 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public boolean isDone() {
+        return this.isDone;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        if(this.isDone) {
+            return "[X] ";
+        }
+        else {
+            return "[ ]";
+        }
+    }
+
+
+}
