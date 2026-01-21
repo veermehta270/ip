d5d1784eae7ca9c53204661793af702863b088b0
Veer Mehta <e1357046@u.nus.edu>
2026-01-21 15:14:42 +0800

    Implement Echo functionality

--- src/main/java/Katappa.java
+++ src/main/java/Katappa.java
@@ -1,18 +1,40 @@
+import java.util.Scanner;
+
 public class Katappa {
+
+    public static final String NAME = "Katappa";
+    public static String LINE = "___________________________________________________________";
+
+    public static void printBoxed(String message) {
+        String line = "===========================================================";
+        System.out.println("\t" + line);
+        System.out.println("\t║ " + message);
+        System.out.println("\t" + line);
+    }
+
     public static void main(String[] args) {
-        String name = "Katappa";
-        String line = "___________________________________________________________";
 
-        System.out.println(line);
-        System.out.println("Hello! I'm Katappa");
-        System.out.println("What can I do for you?");
-        System.out.println();
+        Scanner in = new Scanner(System.in);
 
 
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println();
-        System.out.println(line);
+        System.out.println("\t" + LINE);
+        System.out.println("\t" + "Namaste! My Lord, I'm " + NAME + " your loyal servant");
+        System.out.println("\t" + "How can I serve you today?");
+        System.out.println("\t" + LINE);
+
+       String input = in.nextLine();
+
+
+       while((!input.equalsIgnoreCase("bye"))) {
+           System.out.println("\t" + LINE);
+           System.out.println("\t" + input);
+           System.out.println("\t" + LINE);
+           input = in.nextLine();
+       }
+
+        System.out.println("\t" + LINE);
+        System.out.println("\t" + "Rest peacefully my Lord, Katappa's sword stays ready for your return.");
+        System.out.println("\t" + LINE);
 
     }
 }
