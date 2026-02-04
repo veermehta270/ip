b2e311080c287b01f49c2eedae3dbc87aaea7bb7
Veer Mehta <e1357046@u.nus.edu>
2026-02-04 22:55:00 +0800

    Remove redundant method in Katappa.java and follow coding standards in CommandExecutor

--- src/main/java/Katappa.java
+++ src/main/java/Katappa.java
@@ -5,37 +5,12 @@ public class Katappa {
     private static TaskManager taskManager;
     private static Ui ui;
 
-
-    private static Task classifyTask(String input) {
-        String[] inputArray = input.split(" ");
-
-
-        if (inputArray[0].equalsIgnoreCase("todo")) {
-            String description = input.substring(4);
-            return new Todo(description);
-        } else if (inputArray[0].equalsIgnoreCase("deadline")) {
-            int descriptionStartIndex = 8;
-            int byIndex = input.indexOf("/by");
-            String description = input.substring(descriptionStartIndex, byIndex);
-            String by = input.substring(byIndex + 3);
-            return new Deadline(description, by);
-        } else if (inputArray[0].equalsIgnoreCase("event")) {
-            int descriptionStartIndex = 5;
-            int fromIndex = input.indexOf("/from");
-            int toIndex = input.indexOf("/to");
-            String description = input.substring(descriptionStartIndex, fromIndex);
-            String from = input.substring(fromIndex + 5, toIndex);
-            String to = input.substring(toIndex + 3);
-            return new Event(description, from, to);
-        } else {
-            return new Task(input);
-        }
-    }
-
     public static void main(String[] args) {
 
         startUp();
-        runLoopUntilExit();
+        runLoopUntilExit(
+                
+        );
 
     }
 
